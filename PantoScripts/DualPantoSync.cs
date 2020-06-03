47bbacdd3e1abb57299c599c2746d0c3b7aef160
corinnaj <corinna.jaschek@gmail.com>
2020-06-03 12:57:43 +0200

    remove more logs from panto

--- PantoScripts/DualPantoSync.cs
+++ PantoScripts/DualPantoSync.cs
@@ -99,11 +99,11 @@ public class DualPantoSync : MonoBehaviour
     private static void LogHandler(IntPtr msg)
     {
         String message = Marshal.PtrToStringAnsi(msg);
-        if (message.Contains("Free heap") || message.Contains("Task \"Physics\"") || message.Contains("Task \"I/O\""))
+        if (message.Contains("Free heap") || message.Contains("Task \"Physics\"") || message.Contains("Task \"I/O\"") || message.Contains("Encoder") || message.Contains("SPI"))
         {
             return;
         }
-        else if (message.Contains("failed"))
+        else if (message.Contains("failed") || message.Contains("disconnected"))
         {
             Debug.LogError("[DualPanto] " +  message);
         }
