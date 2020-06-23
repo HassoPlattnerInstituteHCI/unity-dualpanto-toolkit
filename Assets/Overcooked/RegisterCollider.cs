b4b0a82e16dc879710144404cf9550b98410f688
corinnaj <corinna.jaschek@gmail.com>
2020-06-23 13:51:26 +0200

    add overcooked to this repo

---
+++ Assets/Overcooked/RegisterCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class RegisterCollider : MonoBehaviour
+{
+    async void Start()
+    {
+        await Task.Delay(2000);
+        PantoBoxCollider collider = GetComponent<PantoBoxCollider>();
+        collider.CreateObstacle();
+        collider.Enable();
+    }
+}
