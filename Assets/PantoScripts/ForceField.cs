6b718d7da4f326643d64bcaa3b26fcb3752fc024
corinnaj <corinna.jaschek@gmail.com>
2020-07-06 17:23:18 +0200

    ForceField prototype

---
+++ Assets/PantoScripts/ForceField.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceField : MonoBehaviour
+{
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "MeHandle")
+        {
+            GameObject.Find("Panto").GetComponent<UpperHandle>().ApplyForce(new Vector3(1, 0, 0));
+        }
+        else if (other.tag == "ItHandle")
+        {
+            GameObject.Find("Panto").GetComponent<LowerHandle>().ApplyForce(new Vector3(1, 0, 0));
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "MeHandle")
+        {
+            GameObject.Find("Panto").GetComponent<UpperHandle>().StopApplyingForce();
+        }
+        else if (other.tag == "ItHandle")
+        {
+            GameObject.Find("Panto").GetComponent<LowerHandle>().StopApplyingForce();
+        }
+    }
+}
