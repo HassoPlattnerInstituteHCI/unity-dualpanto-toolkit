bf25c80172db36ffb769ead5d8264e1bbf473db0
corinnaj <corinna.jaschek@gmail.com>
2020-07-08 15:29:54 +0200

    very WIP Legend of Panto

---
+++ Assets/LegendOfPanto/Link.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Sleep()
+    {
+        transform.RotateAround();
+        transform.RotateAround();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
