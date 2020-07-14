00a66b7f2262f53e3386b0dae6bf92ea5209a0fc
corinnaj <corinna.jaschek@gmail.com>
2020-07-14 13:20:03 +0200

    small bezier example using PathCreator

---
+++ Assets/ExampleScripts/Bezier.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PathCreation;
+using UnityEngine;
+
+[RequireComponent(typeof(PathCreator))]
+public class Bezier : MonoBehaviour
+{
+
+    public bool closedLoop = true;
+    public Transform[] waypoints;
+
+    void Start()
+    {
+        if (waypoints.Length > 0)
+        {
+            // Create a new bezier path from the waypoints.
+            BezierPath bezierPath = new BezierPath(waypoints, closedLoop, PathSpace.xyz);
+            GetComponent<PathCreator>().bezierPath = bezierPath;
+        }
+    }
+}
