55cc8e0bbf5bf75118dd2e2f44dbf52fa81b707d
corinnaj <corinna.jaschek@gmail.com>
2020-07-15 16:56:09 +0200

    use taskdelay, move declaration of variables

--- Assets/ExampleScripts/MovingObstacleManager.cs
+++ Assets/ExampleScripts/MovingObstacleManager.cs
@@ -8,22 +8,18 @@ public class MovingObstacleManager : MonoBehaviour
     // moving a circular collider at 25Hz.
     // known issue: user can still get into the obstacle
     GameObject obstacle;
-    PantoCollider collider;
-    PantoCollider oldCollider;
     Vector3 direction = new Vector3(0.1f, 0, 0);
     async void Start()
     {
-        // if we register obstacles too early, the device will not work any longer (only sync debug logs will be printed
-        // I am working on fixing this, but for now just add a wait
         obstacle = GameObject.Find("Obstacle");
-        collider = obstacle.GetComponent<PantoCollider>();
-        collider.onUpper = false;
+        PantoCollider collider = obstacle.GetComponent<PantoCollider>();
         await Task.Delay(1000);
-        
+
         collider.CreateObstacle();
         collider.Enable();
-        int moves = 1000;
-        for (int i = 0; i < moves;i++) {
+        while (true)
+        {
+            await Task.Delay(50);
             Vector3 newPos = obstacle.transform.position + direction;
             if (Math.Abs(obstacle.transform.position.x) <= 5 && Math.Abs(newPos.x) > 5)
             {
@@ -34,13 +30,13 @@ public class MovingObstacleManager : MonoBehaviour
     }
 
     async Task MoveObstacle(Vector3 position)
-	{
-        oldCollider = obstacle.GetComponent<PantoCollider>();
+    {
+        PantoCollider oldCollider = obstacle.GetComponent<PantoCollider>();
         GameObject newObs = Instantiate(obstacle);
         Destroy(obstacle);
         obstacle = newObs;
         obstacle.transform.position = position;
-        collider = obstacle.GetComponent<PantoCollider>();
+        PantoCollider collider = obstacle.GetComponent<PantoCollider>();
         collider.CreateObstacle();
 
         // first enable the new collider before removing the old one to make sure the user is not accidentally getting into the obstacle
@@ -48,6 +44,5 @@ public class MovingObstacleManager : MonoBehaviour
         await Task.Delay(20);
         oldCollider.Remove();
         await Task.Delay(20);
-
     }
 }
