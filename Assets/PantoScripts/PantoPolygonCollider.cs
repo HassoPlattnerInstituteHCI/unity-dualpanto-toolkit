ec573e80b83ab2abb40afbf8d5461d89497ca434
Julius Rudolph <julius.rudolph@student.hpi.de>
2020-12-17 18:12:07 +0100

    add gizmos for polyline colliders

--- Assets/PantoScripts/PantoPolygonCollider.cs
+++ Assets/PantoScripts/PantoPolygonCollider.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace DualPantoFramework
 {
     public class PantoPolygonCollider : PantoCollider
@@ -6,6 +8,20 @@ namespace DualPantoFramework
         {
             UpdateId();
             CreatePolygonObstacle();
+        }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Vector2[] points = GetComponent<PolygonCollider2D>().points;
+            Transform parent = this.transform.parent;
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                Vector3 point = transform.TransformPoint(points[i].x, points[i].y, 0);
+                Vector3 nextPoint = transform.TransformPoint(points[i + 1].x, points[i + 1].y, 0);
+                Gizmos.DrawLine(point, nextPoint);
+            }
+
         }
     }
 }
