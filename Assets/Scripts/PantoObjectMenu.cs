f4fad9b166a9c1ededcd91a4289b00f661c56e21
corinnaj <corinna.jaschek@gmail.com>
2021-01-21 11:29:16 +0100

    update menu items

--- Assets/Scripts/PantoObjectMenu.cs
+++ Assets/Scripts/PantoObjectMenu.cs
@@ -1,23 +1,19 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using DualPantoFramework;
+
 public class PantoObjectMenu : MonoBehaviour
 {
-    // Add a menu item named "Do Something" to MyMenu in the menu bar.
-    [MenuItem("DualPanto/Do Something")]
-    static void DoSomething()
-    {
-        Debug.Log("Doing Something...");
-    }
-
     // Add a menu item to create custom GameObjects.
     // Priority 1 ensures it is grouped with the other menu items of the same kind
     // and propagated to the hierarchy dropdown and hierarchy context menus.
-    [MenuItem("GameObject/Panto/Player", false, 10)]
+    [MenuItem("GameObject/Panto/MeHandle", false, 10)]
     static void CreatePantoPlayer(MenuCommand menuCommand)
     {
         // Create a custom game object
         GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        go.AddComponent<Player>();
+        go.name = "MeHandle";
+        go.AddComponent<MeHandle>();
         // Ensure it gets reparented if this was a context click (otherwise does nothing)
         GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
         // Register the creation in the undo system
@@ -30,6 +26,7 @@ public class PantoObjectMenu : MonoBehaviour
     {
         // Create a custom game object
         GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        wall.name = "Wall";
         wall.AddComponent<PantoBoxCollider>();
         // Ensure it gets reparented if this was a context click (otherwise does nothing)
         GameObjectUtility.SetParentAndAlign(wall, menuCommand.context as GameObject);
