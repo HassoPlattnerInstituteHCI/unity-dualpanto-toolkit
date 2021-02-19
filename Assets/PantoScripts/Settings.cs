b83d46539059ab681f931380366f0f0c56410387
corinnaj <corinna.jaschek@gmail.com>
2021-02-19 10:35:54 +0100

    add visualizaion settings

--- Assets/PantoScripts/Settings.cs
+++ Assets/PantoScripts/Settings.cs
@@ -10,6 +10,14 @@ namespace DualPantoFramework
         LEASH
     }
 
+    public enum Visualization
+    {
+        DOOM,
+        BLIND,
+        OUTLINES,
+        DOOM_WITH_OUTLINES
+    }
+
     public class Settings : PantoBehaviour
     {
         public bool tetheringEnabled;
@@ -19,6 +27,7 @@ namespace DualPantoFramework
         public bool pockEnabled;
         public IntroContourStrategy introContourOption;
         public SpeedControlStrategy speedControlOption;
+        public Visualization visualization;
         void UpdateSettings()
         {
             if (!pantoSync.debug)
@@ -26,6 +35,7 @@ namespace DualPantoFramework
                 Debug.Log("updating settings");
                 pantoSync.SetSpeedControl(tetheringEnabled, tetherFactor, innerRadius, outerRadius, speedControlOption, pockEnabled);
                 //SyncSettings(tetherFactor, innerRadius, outerRadius, speedControlOption);
+                //GameObject.Find("PantoManager").GetComponent<PantoManager>().SetVisualization(visualization);
             }
         }
 
