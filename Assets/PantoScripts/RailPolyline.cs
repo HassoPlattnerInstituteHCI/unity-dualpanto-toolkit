e68269216595909861db78fbe423f72bd67201d8
georgt99 <33638816+georgt99@users.noreply.github.com>
2021-02-23 16:23:40 +0100

    add triggers (player needs kinematic rigidbody) and a pretty bad sound

--- Assets/PantoScripts/RailPolyline.cs
+++ Assets/PantoScripts/RailPolyline.cs
@@ -22,8 +22,61 @@ namespace UnityEngine
 
             r.CreateObstacle(points[i], points[i + 1], displacement);
             r.Enable();
+
+            // create new trigger for sound
+            GameObject newObj = new GameObject("RailTrigger");
+            newObj.transform.parent = transform;
+            newObj.transform.position = new Vector3(
+                (points[i].x + points[i + 1].x) / 2,
+                0,
+                (points[i].y + points[i + 1].y) / 2
+            );
+            BoxCollider newTrigger = newObj.AddComponent<BoxCollider>();
+            newTrigger.isTrigger = true;
+            newTrigger.size = new Vector3(
+                displacement*2, // <--- width of trigger
+                10,
+                Vector2.Distance(points[i], points[i + 1])
+            );
+            newObj.transform.LookAt(new Vector3(points[i].x, 0, points[i].y), Vector3.up);
+            newObj.AddComponent<RailTrigger>();
         }
 
+        void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Debug.LogWarning("Player on rail!");
+            }
+        }
     }
+    
 
+    public class RailTrigger : MonoBehaviour
+    {
+        AudioSource audioSource;
+
+        private void Start()
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.clip = Resources.Load<AudioClip>("railSound");
+            audioSource.loop = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                audioSource.Play();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                audioSource.Stop();
+            }
+        }
+    }
 }
