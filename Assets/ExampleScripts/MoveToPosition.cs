0122649070013bc1df662d40384b7f3b1c169735
corinnaj <corinna.jaschek@gmail.com>
2021-03-11 14:12:23 +0100

    retrigger moving in example scene

--- Assets/ExampleScripts/MoveToPosition.cs
+++ Assets/ExampleScripts/MoveToPosition.cs
@@ -6,12 +6,21 @@ public class MoveToPosition : MonoBehaviour
     public bool isUpper;
     public bool shouldFreeHandle;
     public float speed = 10f;
+    PantoHandle handle;
     async void Start()
     {
-        PantoHandle handle = isUpper
+        handle = isUpper
             ? (PantoHandle)GameObject.Find("Panto").GetComponent<UpperHandle>()
             : (PantoHandle)GameObject.Find("Panto").GetComponent<LowerHandle>();
 
         await handle.MoveToPosition(gameObject.transform.position, speed, shouldFreeHandle);
+    }
+
+    async void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            await handle.MoveToPosition(gameObject.transform.position, speed, shouldFreeHandle);
+        }
     }
 }
