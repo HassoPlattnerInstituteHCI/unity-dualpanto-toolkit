4a7ced3cf99dc37a56336739a58de2d6e34e1b8b
corinnaj <corinna.jaschek@gmail.com>
2021-03-14 13:07:56 +0100

    add perception cone

---
+++ Assets/PantoScripts/RoomElement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DualPantoFramework
+{
+    [RequireComponent(typeof(AudioSource))]
+    [RequireComponent(typeof(Collider))]
+    public class RoomElement : MonoBehaviour
+    {
+        AudioSource audioSource;
+        public LayerMask layerMask;
+
+        void Awake() {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.loop = true;
+            audioSource.playOnAwake = false;
+        }
+
+        protected bool IsViewClear(Collider collider)
+        {
+            Vector3 handlePosition = collider.transform.position;
+            Vector3 direction = transform.position - handlePosition;
+            RaycastHit hit;
+            if (Physics.Raycast(handlePosition, direction, out hit, direction.magnitude, layerMask)) return false;
+            else return true;
+        }
+
+        void OnTriggerEnter(Collider collider)
+        {
+            Debug.Log("enter" + collider.name);
+            if (collider.tag == "PerceptionCone" && IsViewClear(collider))
+            {
+                if (audioSource.isPlaying) return;
+                if (audioSource.enabled) audioSource.Play();
+            }
+        }
+
+        void OnTriggerStay(Collider collider)
+        {
+            if (collider.tag == "PerceptionCone" && IsViewClear(collider))
+            {
+                if (audioSource.isPlaying) return;
+                if (audioSource.enabled) audioSource.Play();
+            } else if (collider.tag == "PerceptionCone") {
+                audioSource.Pause();
+            }
+        }
+
+        void OnTriggerExit(Collider collider)
+        {
+            audioSource.Pause();
+        }
+    }
+}
