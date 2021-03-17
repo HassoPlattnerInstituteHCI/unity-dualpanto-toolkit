7c63e0bae475c0b346bc613ebb5a45fc8fbfab69
Julius Rudolph <julius.rudolph@student.hpi.de>
2021-03-17 17:55:03 +0100

    let me handle rotate in rotation demo scene

--- Assets/ExampleScripts/RotationMe.cs
+++ Assets/ExampleScripts/RotationMe.cs
@@ -12,5 +12,7 @@ public class RotationMe : MonoBehaviour
     void FixedUpdate()
     {
         transform.eulerAngles = new Vector3(0, upperHandle.GetRotation(), 0);
+        transform.RotateAround(transform.position, Vector3.up, 0.5f);
+        upperHandle.Rotate(transform.eulerAngles.y);
     }
 }
